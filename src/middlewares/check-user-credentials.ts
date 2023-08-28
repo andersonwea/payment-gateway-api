ad1695c10b06dbed0eb8e354b9fcc88f0b71e2f7
chain <klaus.mika1052@gmail.com>
2023-08-28 19:22:22 -0300

    create middleware to check user credentials

---
+++ src/middlewares/check-user-credentials.ts
@@ -0,0 +1,49 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { env } from '../env'
+import { pool } from '../lib/database'
+import { DecodeResult } from '../@types'
+
+export async function checkUserCredentials(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const token = request.cookies.token
+
+  if (!token) {
+    return response.status(401).json({ message: 'Unauthorized' })
+  }
+
+  try {
+    let decoded: any
+
+    try {
+      decoded = jwt.verify(token, env.JWT_SECRET)
+    } catch (err) {
+      return response.status(401).json({ message: 'Unauthorized.' })
+    }
+
+    const { id } = decoded
+
+    if (id === 'invalid token') {
+      return response.status(401).json({ message: 'Unauthorized.' })
+    }
+
+    const query = `
+      SELECT id, name, email 
+      FROM users
+      WHERE id = $1
+    `
+
+    const user = await pool.query(query, [id])
+
+    response.locals.user = user.rows[0]
+
+    return next()
+  } catch (err: any) {
+    console.error(err.message)
+
+    return response.status(500).json({ message: 'Internal Server Error.' })
+  }
+}
